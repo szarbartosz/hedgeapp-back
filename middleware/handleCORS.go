653a49b4374171801fc7d3ed96ed0778182b22b8
Bartosz Szar <szarbartosz@gmail.com>
2023-06-19 11:13:23 +0200

    add http DELETE to allowed methods

--- middleware/handleCORS.go
+++ middleware/handleCORS.go
@@ -7,7 +7,7 @@ func HandleCORS() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Set-Cookie")
 
 		if c.Request.Method == "OPTIONS" {
