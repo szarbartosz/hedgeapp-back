14e0916311d1d067fa558397910ec2e413eb8ca6
Bartosz Szar <szarbartosz@gmail.com>
2023-06-23 21:47:06 +0200

    bring SameSiteLaxMode back

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -97,8 +97,7 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	// c.SetSameSite(http.SameSiteLaxMode)
-	c.SetSameSite(http.SameSiteNoneMode)
+	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie("Authorization", tokenString, 60*60*24*30, "/", os.Getenv("COOKIE_DOMAIN"), os.Getenv("SECURE_COOKIE") == "true", true)
 
 	c.JSON(http.StatusOK, gin.H{
