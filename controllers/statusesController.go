c37c4d11218f82a3231e75bdaf32fae59dcc3a4f
Bartosz Szar <szarbartosz@gmail.com>
2024-08-04 23:36:48 +0200

    feat: implement missing crud handlers for investors locations and statuses

--- controllers/statusesController.go
+++ controllers/statusesController.go
@@ -56,6 +56,30 @@ func UpdateStatus(c *gin.Context) {
 	c.JSON(http.StatusOK, updatedStatus)
 }
 
+func UpdateStatus(c *gin.Context) {
+	var body models.Status
+
+	if c.BindJSON(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request body",
+		})
+		return
+	}
+
+	result := initializers.DB.Where("id", c.Param("id")).Updates(&body)
+
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to update status",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Status updated successfully",
+	})
+}
+
 func GetStatuses(c *gin.Context) {
 	var statuses []models.Status
 
@@ -69,6 +93,32 @@ func GetStatuses(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, statuses)
+}
+
+func DeleteStatus(c *gin.Context) {
+	var status models.Status
+
+	result := initializers.DB.First(&status, c.Param("id"))
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Status not found",
+		})
+		return
+	}
+
+	deleteResult := initializers.DB.Delete(&status)
+
+	if deleteResult.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to delete investor: " + deleteResult.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Status deleted",
+	})
 }
 
 func DeleteStatus(c *gin.Context) {
