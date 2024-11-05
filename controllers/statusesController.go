444aeb135d037f2d2e55c95d74b7d2130b414fe2
Bartosz Szar <szarbartosz@gmail.com>
2024-11-05 23:24:09 +0100

    remove duplicated functions

--- controllers/statusesController.go
+++ controllers/statusesController.go
@@ -56,31 +56,6 @@ func UpdateStatus(c *gin.Context) {
 	c.JSON(http.StatusOK, updatedStatus)
 }
 
-func UpdateStatus(c *gin.Context) {
-	var body models.Status
-
-	if c.BindJSON(&body) != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid request body",
-		})
-		return
-	}
-
-	result := initializers.DB.Where("id", c.Param("id")).Updates(&body)
-
-	if result.RowsAffected == 0 {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to update status",
-		})
-		return
-	}
-
-	var updatedStatus models.Status
-	initializers.DB.First(&updatedStatus, c.Param("id"))
-
-	c.JSON(http.StatusOK, updatedStatus)
-}
-
 func GetStatuses(c *gin.Context) {
 	var statuses []models.Status
 
@@ -94,32 +69,6 @@ func GetStatuses(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, statuses)
-}
-
-func DeleteStatus(c *gin.Context) {
-	var status models.Status
-
-	result := initializers.DB.First(&status, c.Param("id"))
-
-	if result.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Status not found",
-		})
-		return
-	}
-
-	deleteResult := initializers.DB.Delete(&status)
-
-	if deleteResult.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to delete investor: " + deleteResult.Error.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Status deleted",
-	})
 }
 
 func DeleteStatus(c *gin.Context) {
